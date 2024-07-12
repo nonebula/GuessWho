5a1b9f824eb7fc0a4460882b5ad2ea69556de2b5
Joe Higgs <joe.higgs@mercator.group>
2024-07-12 10:22:54 +0100

    Character case class & characters

---
+++ src/main/scala/Character.scala
@@ -0,0 +1,25 @@
+case class Character(val name: String, val gender: String, val hairColor: String, val eyeColor: String, val wearsGlasses: Boolean, val facialHair: Boolean)
+
+//val characters: List[Character] = List[String, String, String, String, Boolean, Boolean]
+
+Character ("joe", "male", "brown", "blue", wearsGlasses = false, facialHair = false)
+Character ("muhammad", "male", "black", "brown", wearsGlasses = true, facialHair = true)
+Character ("april", "female", "blonde", "blue", wearsGlasses = true, facialHair = false)
+Character ("sally", "female", "blue", "green", wearsGlasses = true, facialHair = true)
+Character ("spencer", "male", "brown", "brown", wearsGlasses = false, facialHair = true)
+Character ("gemma", "female", "ginger", "Blue", wearsGlasses = true, facialHair = false)
+Character ("jamie", "male", "none", "green", wearsGlasses = true, facialHair = true)
+Character ("jessica", "female", "green", "green", wearsGlasses = false, facialHair = false)
+Character ("bilal", "male", "brown", "green", wearsGlasses = false, facialHair = true)
+Character ("lisa", "female", "black", "brown", wearsGlasses = true, facialHair = false)
+Character ("tom", "male", "brown", "blue", wearsGlasses = true, facialHair = false)
+Character ("cheryl", "female", "blonde", "green", wearsGlasses = false, facialHair = false)
+Character ("arei", "male", "brown", "brown", wearsGlasses = false, facialHair = true)
+Character ("kelly", "female", "none", "green", wearsGlasses = false, facialHair = false)
+Character ("tayamul", "male", "brown", "blue", wearsGlasses = false, facialHair = false)
+Character ("elaine", "female", "ginger", "blue", wearsGlasses = false, facialHair = true)
+Character ("roshan", "male", "brown", "blue", wearsGlasses = true, facialHair = false)
+Character ("patricia", "female", "purple", "yellow", wearsGlasses = false, facialHair = false)
+Character ("dave", "male", "none", "yellow", wearsGlasses = false, facialHair = false)
+Character ("bobbiana", "female", "Brown", "Blue", wearsGlasses = true, facialHair = false)
+
