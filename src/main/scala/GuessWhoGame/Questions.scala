abff432c8c5340a67e747b9566a18b8d4126c096
Joe Higgs <joe.higgs@mercator.group>
2024-07-15 11:02:47 +0100

    Question handling refactored

--- src/main/scala/GuessWhoGame/Questions.scala
+++ src/main/scala/GuessWhoGame/Questions.scala
@@ -1,27 +1,72 @@
 class Questions {
-  def askQuestion(attribute: String, value: Either[String, Boolean]): Unit = {
+  def handleQuestion(attribute: String, value: Either[String, Boolean]): Unit = {
     attribute match {
       case "name" =>
-        if (selectedCharacter.name == value) remainingCharacters.filter(_.name == value) // game end here!
-        else remainingCharacters = remainingCharacters.filterNot(_.name == value)
-        //trigger game end
+        value match {
+          case Left(value) =>
+            if (selectedCharacter.name == value) {
+              remainingCharacters.filter(_.name == value) // game end here!
+              endGame()
+            } else {
+              remainingCharacters = remainingCharacters.filterNot(_.name == value)
+            }
+          case _ => println("Invalid name value added. Try again")
+        }
       case "gender" =>
-        if (selectedCharacter.gender == value) remainingCharacters.filter(_.gender == value)
-        else remainingCharacters = remainingCharacters.filterNot(_.gender == value)
+        value match {
+          case Left(value) =>
+            if (selectedCharacter.gender == value) {
+              remainingCharacters.filter(_.gender == value)
+            } else {
+              remainingCharacters = remainingCharacters.filterNot(_.gender == value)
+            }
+          case _ => println("Invalid gender value added. Try again")
+        }
       case "hairColor" =>
-        if (selectedCharacter.hairColor == value) remainingCharacters.filter(_.hairColor == value)
-        else remainingCharacters = remainingCharacters.filterNot(_.hairColor == value)
+        value match {
+          case Left(value) =>
+            if (selectedCharacter.hairColor == value) {
+              remainingCharacters.filter(_.hairColor == value)
+            }
+            else {
+              remainingCharacters = remainingCharacters.filterNot(_.hairColor == value)
+            }
+          case _ => println("Invalid hair colour value added. Try again")
+        }
       case "eyeColor" =>
-        if (selectedCharacter.eyeColor == value) remainingCharacters.filter(_.eyeColor == value)
-        else remainingCharacters = remainingCharacters.filterNot(_.eyeColor == value)
+        value match {
+          case Left(value) =>
+            if (selectedCharacter.eyeColor == value) {
+              remainingCharacters.filter(_.eyeColor == value)
+            }
+            else {
+              remainingCharacters = remainingCharacters.filterNot(_.eyeColor == value)
+            }
+          case _ => println("Invalid eye colour value added. Try again")
+        }
       case "wearsGlasses" =>
-        if (selectedCharacter.wearsGlasses == value) remainingCharacters.filter(_.wearsGlasses == value)
-        else remainingCharacters = remainingCharacters.filterNot(_.wearsGlasses == value)
+        value match {
+          case Right(value) =>
+            if (selectedCharacter.wearsGlasses == value) {
+              remainingCharacters.filter(_.wearsGlasses == value)
+            }
+            else {
+              remainingCharacters = remainingCharacters.filterNot(_.wearsGlasses == value)
+            }
+          case _ => println("Invalid glasses value added. Try again")
+        }
       case "hasFacialHair" =>
-        if (selectedCharacter.hasFacialHair == value) remainingCharacters.filter(_.hasFacialHair == value)
-        else remainingCharacters = remainingCharacters.filterNot(_.hasFacialHair == value)
+        value match {
+          case Right(value) =>
+            if (selectedCharacter.hasFacialHair == value) {
+              remainingCharacters.filter(_.hasFacialHair == value)
+            }
+            else {
+              remainingCharacters = remainingCharacters.filterNot(_.hasFacialHair == value)
+            }
+          case _ => println("Invalid facial hair value added. Try again")
+        }
 
-      //need for an error case here??
     }
   }
 }
