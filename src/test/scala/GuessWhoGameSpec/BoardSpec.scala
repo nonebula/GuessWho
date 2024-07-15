f8565066576eed34274c801c56e02f2a4a355aa6
Muhammad Mulla <muhammad.mulla@mercator.group>
2024-07-15 15:57:39 +0100

    BoardSpec hair colour test fixed

--- src/test/scala/GuessWhoGameSpec/BoardSpec.scala
+++ src/test/scala/GuessWhoGameSpec/BoardSpec.scala
@@ -9,6 +9,23 @@ class BoardSpec extends AnyFlatSpec {
   val characters = List(
     Character("joe", "male", "brown", "blue", wearsGlasses = false, facialHair = false),
     Character("muhammad", "male", "black", "brown", wearsGlasses = true, facialHair = true),
+    Character("april", "female", "blonde", "blue", wearsGlasses = true, facialHair = false),
+    Character("sally", "female", "blue", "green", wearsGlasses = true, facialHair = true),
+    Character("spencer", "male", "brown", "brown", wearsGlasses = false, facialHair = true),
+    Character("gemma", "female", "ginger", "blue", wearsGlasses = true, facialHair = false),
+    Character("jamie", "male", "none", "green", wearsGlasses = true, facialHair = true),
+    Character("jessica", "female", "green", "green", wearsGlasses = false, facialHair = false),
+    Character("bilal", "male", "brown", "green", wearsGlasses = false, facialHair = true),
+    Character("lisa", "female", "black", "brown", wearsGlasses = true, facialHair = false),
+    Character("tom", "male", "brown", "blue", wearsGlasses = true, facialHair = false),
+    Character("cheryl", "female", "blonde", "green", wearsGlasses = false, facialHair = false),
+    Character("arei", "male", "brown", "brown", wearsGlasses = false, facialHair = true),
+    Character("kelly", "female", "none", "green", wearsGlasses = false, facialHair = false),
+    Character("tayamul", "male", "brown", "blue", wearsGlasses = false, facialHair = false),
+    Character("elaine", "female", "ginger", "blue", wearsGlasses = false, facialHair = true),
+    Character("roshan", "male", "brown", "blue", wearsGlasses = true, facialHair = false),
+    Character("patricia", "female", "purple", "yellow", wearsGlasses = false, facialHair = false),
+    Character("dave", "male", "none", "yellow", wearsGlasses = false, facialHair = false),
     Character("bobbiana", "female", "brown", "blue", wearsGlasses = true, facialHair = false)
   )
 
