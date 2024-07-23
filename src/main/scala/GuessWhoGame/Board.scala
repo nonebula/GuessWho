851ee315bb8b1b69cb4b02e9a18bef6dd7a62035
Joe Higgs <joe.higgs@mercator.group>
2024-07-23 13:12:13 +0100

    Board logic refactored

--- src/main/scala/GuessWhoGame/Board.scala
+++ src/main/scala/GuessWhoGame/Board.scala
@@ -1,34 +1,38 @@
 package GuessWhoGame
 
 import scala.util.Random
+import GuessWhoGame.character._
 
 //values could be more domain specific, name/selecting could be within character case class, then able to test separately
-case class Board(characters: List[Character]) {
-  def printCharacterNames(): Unit = {
-    characters.foreach(Character => println(Character.name))
+class Board(private val characters: List[Character], private val selectedCharacter: Character) {
+  def printCharacterNames(): List[String] = {
+    //    characters.foreach(Character => println(Character.name))
+    characters.map(_.name)
   }
 
-  val selectedCharacter: Character = selectRandomCharacter(characters)
-  var remainingCharacters: List[Character] = characters
+  //  val selectedCharacter: Character = selectRandomCharacter(characters)
+  //  var remainingCharacters: List[Character] = characters
 
-  def printRemainingCharacters(): Unit = {
-    remainingCharacters.foreach(Character => println(Character.name))
+  def printRemainingCharacters(remainingCharacters: List[Character]): Unit = {
+    //    remainingCharacters.foreach(Character => println(Character.name))
+    remainingCharacters.map(_.name)
   }
+
+  def getRemainingCharacters(remainingCharacters: List[Character]): List[Character] = remainingCharacters
+
+  def checkWinCondition(remainingCharacters: List[Character]): Boolean = remainingCharacters.size == 1
 
   def selectRandomCharacter(characters: List[Character]): Character = {
     characters(Random.nextInt(characters.size))
   }
 
-  //Testing purposes
-  def getSelectedCharacter: Character = selectedCharacter
+  //  //Testing purposes
+  //  def getSelectedCharacter: Character = selectedCharacter
+  //
+  //  def printSelectedCharacter(): Unit = {
+  //    println(selectedCharacter.name)
+  //  }
 
-  def printSelectedCharacter(): Unit = {
-    println(selectedCharacter.name)
-  }
-
-  def getRemainingCharacters: List[Character] = remainingCharacters
-
-  def checkWinCondition: Boolean = getRemainingCharacters.size == 1
 
   //Future development
   //  def resetBoard(): Unit = {
@@ -39,78 +43,72 @@ case class Board(characters: List[Character]) {
   //    var remainingCharacters = characters.filterNot(_ == character)
   //  }
 
-  def handleQuestion(attribute: String, value: Either[String, Boolean]): Unit = {
-    remainingCharacters = attribute match {
-      case "name" =>
-        value match {
-          case Left(value) =>
-            if (selectedCharacter.name == value) {
-              remainingCharacters.filter(_.name == value)
-            } else {
-              remainingCharacters.filterNot(_.name == value)
-            }
-          case _ => println("Invalid name value added. Try again")
-            remainingCharacters
-        }
-      case "gender" =>
-        value match {
-          case Left(value) =>
-            if (selectedCharacter.gender == value) {
-              remainingCharacters.filter(_.gender == value)
-            } else {
-              remainingCharacters.filterNot(_.gender == value)
-            }
-          case _ => println("Invalid gender value added. Try again")
-            remainingCharacters
-        }
-      case "hairColor" =>
-        value match {
-          case Left(value) =>
-            if (selectedCharacter.hairColor == value) {
-              remainingCharacters.filter(_.hairColor == value)
-            }
-            else {
-              remainingCharacters.filterNot(_.hairColor == value)
-            }
-          case _ => println("Invalid hair colour value added. Try again")
-            remainingCharacters
-        }
-      case "eyeColor" =>
-        value match {
-          case Left(value) =>
-            if (selectedCharacter.eyeColor == value) {
-              remainingCharacters.filter(_.eyeColor == value)
-            }
-            else {
-              remainingCharacters.filterNot(_.eyeColor == value)
-            }
-          case _ => println("Invalid eye colour value added. Try again")
-            remainingCharacters
-        }
-      case "wearsGlasses" =>
-        value match {
-          case Right(value) =>
-            if (selectedCharacter.wearsGlasses == value) {
-              remainingCharacters.filter(_.wearsGlasses == value)
-            }
-            else {
-              remainingCharacters.filterNot(_.wearsGlasses == value)
-            }
-          case _ => println("Invalid glasses value added. Try again")
-            remainingCharacters
-        }
-      case "hasFacialHair" =>
-        value match {
-          case Right(value) =>
-            if (selectedCharacter.facialHair == value) {
-              remainingCharacters.filter(_.facialHair == value)
-            }
-            else {
-              remainingCharacters.filterNot(_.facialHair == value)
-            }
-          case _ => println("Invalid facial hair value added. Try again")
-            remainingCharacters
-        }
+  def handleQuestion(attribute: String, value: Either[String, Boolean], remainingCharacters: List[Character]): List[Character] = {
+    attribute match {
+      case "name" => filterByName(value, remainingCharacters)
+      case "gender" => filterByGender(value, remainingCharacters)
+      case "hairColor" => filterByHairColor(value, remainingCharacters)
+      case "eyeColor" => filterByEyeColor(value, remainingCharacters)
+      case "wearsGlasses" => filterByWearGlasses(value, remainingCharacters)
+      case "hasFacialHair" => filterByFacialHair(value, remainingCharacters)
+      case _ =>
+        println("Invalid attribute. Try again.")
+        remainingCharacters
     }
   }
+
+  private def filterByName(value: Either[String, Boolean], remainingCharacters: List[Character]): List[Character] = {
+    value match {
+      case Left(name) => remainingCharacters.filter(_.name == name)
+      case _ =>
+        println("Invalid name value added. Try again.")
+        remainingCharacters
+    }
+  }
+
+  private def filterByGender(value: Either[String, Boolean], remainingCharacters: List[Character]): List[Character] = {
+    value match {
+      case Left(gender) => remainingCharacters.filter(_.gender.toString == gender)
+      case _ =>
+        println("Invalid gender value added. Try again.")
+        remainingCharacters
+    }
+  }
+
+  private def filterByHairColor(value: Either[String, Boolean], remainingCharacters: List[Character]): List[Character] = {
+    value match {
+      case Left(hairColor) => remainingCharacters.filter(_.hairColor.toString == hairColor)
+      case _ =>
+        println("Invalid hair color value added. Try again.")
+        remainingCharacters
+    }
+  }
+
+  private def filterByEyeColor(value: Either[String, Boolean], remainingCharacters: List[Character]): List[Character] = {
+    value match {
+      case Left(eyeColor) => remainingCharacters.filter(_.eyeColor.toString == eyeColor)
+      case _ =>
+        println("Invalid eye color value added. Try again.")
+        remainingCharacters
+    }
+  }
+
+  private def filterByWearGlasses(value: Either[String, Boolean], remainingCharacters: List[Character]): List[Character] = {
+    value match {
+      case Right(wearsGlasses) => remainingCharacters.filter(_.wearsGlasses.exists(_.value == wearsGlasses))
+      case _ =>
+        println("Invalid glasses value added. Try again.")
+        remainingCharacters
+    }
+  }
+
+  private def filterByFacialHair(value: Either[String, Boolean], remainingCharacters: List[Character]): List[Character] = {
+    value match {
+      case Right(facialHair) => remainingCharacters.filter(_.facialHair.exists(_.value == facialHair))
+      case _ =>
+        println("Invalid facial hair value added. Try again.")
+        remainingCharacters
+    }
+  }
+
 }
