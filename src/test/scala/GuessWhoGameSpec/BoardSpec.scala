28c3fa4271c56aad2b23e0bed18dc07bef513050
joe <147166151+nonebula@users.noreply.github.com>
2024-09-09 12:47:43 +0100

    space

--- src/test/scala/GuessWhoGameSpec/BoardSpec.scala
+++ src/test/scala/GuessWhoGameSpec/BoardSpec.scala
@@ -1,182 +1,159 @@
 package GuessWhoGameSpec
 
 import GuessWhoGame._
-import GuessWhoGame.character.{CharacterManager, EyeColor, Gender, HairColor, PersonWithFacialHair, PersonWithGlasses}
+import GuessWhoGame.character.{CharacterManager, Character, EyeColor, Gender, HairColor, PersonWithFacialHair, PersonWithGlasses}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
 
 class BoardSpec extends AnyFlatSpec {
-  
-  //prints out characters onto the board
-  "gameBoard" should "initialise with the provided characters" in {
+
+  // Sample characters for testing
+  val characters: List[Character] = CharacterManager.createCharacters()
+
+  // Initializes the board with the provided characters
+  "Board" should "initialize with the provided characters" in {
     val gameBoard = new Board(characters)
     gameBoard.getRemainingCharacters shouldEqual characters
   }
 
   // Selects a character
-  it should "select a random character at the start of the game and keeps this character in the list" in {
+  it should "select a random character at the start of the game and keep this character in the list" in {
     val gameBoard = new Board(characters)
     characters should contain(gameBoard.getSelectedCharacter)
-    gameBoard.getSelectedCharacter shouldEqual (gameBoard.selectedCharacter)
-    //assert(gameBoard.selectRandomCharacter(characters: List[Character]) == gameBoard.selectedCharacter)
   }
 
-  //Watches for the win condition
+  // Checks the win condition
   it should "check win condition correctly" in {
     val gameBoard = new Board(characters)
     if (characters.size <= 1) {
-      assert(gameBoard.checkWinCondition)
+      assert(gameBoard.checkWinCondition())
     } else {
-      assert(!gameBoard.checkWinCondition)
+      assert(!gameBoard.checkWinCondition())
     }
   }
 
-  //Handles questions, re-renders based on question result && Stores remaining characters
+  // Handles questions and eliminates characters based on attributes
   it should "eliminate characters correctly based on gender questions" in {
     val gameBoard = new Board(characters)
-    val initialSize = gameBoard.remainingCharacters.size
+    val initialSize = gameBoard.getRemainingCharacters.size
     gameBoard.handleQuestion("gender", Left("male"))
-    val newSize = gameBoard.remainingCharacters.size
+    val newSize = gameBoard.getRemainingCharacters.size
     newSize should be < initialSize
-    //Future testcases
-    //      gameBoard.getRemainingCharacters.forall(_.gender == "male") shouldBe false
   }
 
   it should "eliminate characters correctly based on name questions" in {
     val gameBoard = new Board(characters)
-    val initialSize = gameBoard.remainingCharacters.size
+    val initialSize = gameBoard.getRemainingCharacters.size
     gameBoard.handleQuestion("name", Left("muhammad"))
-    val newSize = gameBoard.remainingCharacters.size
+    val newSize = gameBoard.getRemainingCharacters.size
     newSize should be < initialSize
   }
 
-  it should "eliminate characters correctly based on hairColor questions" in {
+  it should "eliminate characters correctly based on hair color questions" in {
     val gameBoard = new Board(characters)
-    val initialSize = gameBoard.remainingCharacters.size
+    val initialSize = gameBoard.getRemainingCharacters.size
     gameBoard.handleQuestion("hairColor", Left("blonde"))
-    val newSize = gameBoard.remainingCharacters.size
+    val newSize = gameBoard.getRemainingCharacters.size
     newSize should be < initialSize
   }
 
-  it should "eliminate characters correctly based on eyeColor questions" in {
+  it should "eliminate characters correctly based on eye color questions" in {
     val gameBoard = new Board(characters)
-    val initialSize = gameBoard.remainingCharacters.size
+    val initialSize = gameBoard.getRemainingCharacters.size
     gameBoard.handleQuestion("eyeColor", Left("blue"))
-    val newSize = gameBoard.remainingCharacters.size
+    val newSize = gameBoard.getRemainingCharacters.size
     newSize should be < initialSize
   }
 
-  it should "eliminate characters correctly based on wearsGlasses questions" in {
+  it should "eliminate characters correctly based on wears glasses questions" in {
     val gameBoard = new Board(characters)
-    val initialSize = gameBoard.remainingCharacters.size
-    gameBoard.handleQuestion("wearsGlasses", Right(false))
-    val newSize = gameBoard.remainingCharacters.size
+    val initialSize = gameBoard.getRemainingCharacters.size
+    gameBoard.handleQuestion("wearsGlasses", Right(true))
+    val newSize = gameBoard.getRemainingCharacters.size
     newSize should be < initialSize
   }
 
-  it should "eliminate characters correctly based on HasFacialHair questions" in {
+  it should "eliminate characters correctly based on facial hair questions" in {
     val gameBoard = new Board(characters)
-    val initialSize = gameBoard.remainingCharacters.size
-    gameBoard.handleQuestion("hasFacialHair", Right(false))
-    val newSize = gameBoard.remainingCharacters.size
+    val initialSize = gameBoard.getRemainingCharacters.size
+    gameBoard.handleQuestion("hasFacialHair", Right(true))
+    val newSize = gameBoard.getRemainingCharacters.size
     newSize should be < initialSize
   }
 
+  // Handles incorrect attribute values cleanly
   it should "handle incorrect attribute values cleanly" in {
-    val gameBoard = Board(characters)
+    val gameBoard = new Board(characters)
     val initialSize = gameBoard.getRemainingCharacters.size
     gameBoard.handleQuestion("gender", Left("nonexistent gender"))
     val newSize = gameBoard.getRemainingCharacters.size
-
     newSize shouldEqual initialSize
   }
 
+  // Handles edge case where all characters have the same attribute
   it should "handle edge case where all characters have the same attribute" in {
     val sameGenderCharacters = List(
-      Character("john", "male", "brown", "blue", wearsGlasses = false, facialHair = false),
-      Character("paul", "male", "black", "green", wearsGlasses = true, facialHair = true)
+      Character("john", Gender.Male, HairColor.Brown, EyeColor.Blue, Some(PersonWithGlasses(false)), Some(PersonWithFacialHair(false))),
+      Character("paul", Gender.Male, HairColor.Black, EyeColor.Green, Some(PersonWithGlasses(true)), Some(PersonWithFacialHair(true)))
     )
-    val gameBoard = Board(sameGenderCharacters)
+    val gameBoard = new Board(sameGenderCharacters)
     gameBoard.handleQuestion("gender", Left("male"))
     gameBoard.getRemainingCharacters.size shouldEqual 2
   }
 
-  it should "fail to eliminate characters with incorrect gender" in {
-    val gameBoard = Board(characters)
-    gameBoard.handleQuestion("gender", Left("female"))
-    gameBoard.getRemainingCharacters.forall(_.gender == "male") shouldBe true
+  // Prints character names
+  it should "print character names" in {
+    val gameBoard = new Board(characters)
+    val characterNames = gameBoard.printCharacterNames()
+    characterNames.length should be(20)
+    characterNames should contain(characters.head.name)
   }
-}
 
+  // Handles hair color question correctly
+  it should "handle hair color question correctly" in {
+    val gameBoard = new Board(characters)
+    val remainingCharacters = gameBoard.handleQuestion("hairColor", Left("brown"))
+    remainingCharacters shouldBe a[List[_]]
+    all(remainingCharacters.map(_.hairColor)) shouldEqual HairColor.Brown
+  }
 
+  // Handles eye color question correctly
+  it should "handle eye color question correctly" in {
+    val gameBoard = new Board(characters)
+    val remainingCharacters = gameBoard.handleQuestion("eyeColor", Left("blue"))
+    remainingCharacters shouldBe a[List[_]]
+    all(remainingCharacters.map(_.eyeColor)) shouldEqual EyeColor.Blue
+  }
 
-// future development
-//  it should "eliminate a character correctly" in {
-//    val gameBoard = new Board(characters)
-//    val characterToEliminate = characters.head
-//    gameBoard.eliminateCharacter(characterToEliminate)
-//    gameBoard.getRemainingCharacters should not contain characterToEliminate
-//    gameBoard.getRemainingCharacters.size shouldEqual (characters.size - 1)
-//  }
-//
-//  it should "return an empty list if all characters are eliminated" in {
-//    val gameBoard = Board(characters)
-//    characters.foreach(gameBoard.eliminateCharacter)
-//    gameBoard.getRemainingCharacters shouldBe empty
-//  }
+  // Handles gender question correctly
+  it should "handle gender question correctly" in {
+    val gameBoard = new Board(characters)
+    val remainingCharacters = gameBoard.handleQuestion("gender", Left("male"))
+    remainingCharacters shouldBe a[List[_]]
+    all(remainingCharacters.map(_.gender)) shouldEqual Gender.Male
+  }
 
-"Board" should "print character names" in {
-  val characters = CharacterManager.createCharacters()
-  val board = new Board(characters, characters.head)
-  val characterNames = board.printCharacterNames()
-  characterNames.length should be(20)
-  characterNames should contain(characters.head.name)
-}
+  // Handles wears glasses question correctly
+  it should "handle wears glasses question correctly" in {
+    val gameBoard = new Board(characters)
+    val remainingCharacters = gameBoard.handleQuestion("wearsGlasses", Right(true))
+    remainingCharacters shouldBe a[List[_]]
+    all(remainingCharacters.flatMap(_.wearsGlasses)) shouldEqual PersonWithGlasses(true)
+  }
 
-"Board" should "handle hair color question correctly" in {
-  val characters = CharacterManager.createCharacters()
-  val board = new Board(characters, characters.head)
-  val remainingCharacters = board.handleQuestion("haircolor", Left("brown"), characters)
-  remainingCharacters shouldBe a[List[_]]
-  all(remainingCharacters.map(_.hairColor)) shouldBe HairColor.Brown
-}
+  // Handles facial hair question correctly
+  it should "handle facial hair question correctly" in {
+    val gameBoard = new Board(characters)
+    val remainingCharacters = gameBoard.handleQuestion("hasFacialHair", Right(true))
+    remainingCharacters shouldBe a[List[_]]
+    all(remainingCharacters.flatMap(_.facialHair)) shouldEqual PersonWithFacialHair(true)
+  }
 
-"Board" should "handle eye color question correctly" in {
-  val characters = CharacterManager.createCharacters()
-  val board = new Board(characters, characters.head)
-  val remainingCharacters = board.handleQuestion("eyecolor", Left("blue"), characters)
-  remainingCharacters shouldBe a[List[_]]
-  all(remainingCharacters.map(_.eyeColor)) shouldBe EyeColor.Blue
-}
-
-"Board" should "handle gender question correctly" in {
-  val characters = CharacterManager.createCharacters()
-  val board = new Board(characters, characters.head)
-  val remainingCharacters = board.handleQuestion("gender", Left("male"), characters)
-  remainingCharacters shouldBe a[List[_]]
-  all(remainingCharacters.map(_.gender)) shouldBe Gender.Male
-}
-
-"Board" should "handle wears glasses question correctly" in {
-  val characters = CharacterManager.createCharacters()
-  val board = new Board(characters, characters.head)
-  val remainingCharacters = board.handleQuestion("wearsglasses", Right(true), characters)
-  remainingCharacters shouldBe a[List[_]]
-  all(remainingCharacters.flatMap(_.wearsGlasses)) shouldBe PersonWithGlasses(true)
-}
-
-"Board" should "handle facial hair question correctly" in {
-  val characters = CharacterManager.createCharacters()
-  val board = new Board(characters, characters.head)
-  val remainingCharacters = board.handleQuestion("hasfacialhair", Right(true), characters)
-  remainingCharacters shouldBe a[List[_]]
-  all(remainingCharacters.flatMap(_.facialHair)) shouldBe PersonWithFacialHair(true)
-}
-
-"Board" should "handle name guess correctly" in {
-  val characters = CharacterManager.createCharacters()
-  val selectedCharacter = characters.head
-  val board = new Board(characters, selectedCharacter)
-  val remainingCharacters = board.handleQuestion("name", Left(selectedCharacter.name.toLowerCase()), characters)
-  board.checkWinCondition(remainingCharacters) should be(true)
+  // Handles name guess correctly
+  it should "handle name guess correctly" in {
+    val selectedCharacter = characters.head
+    val gameBoard = new Board(characters, selectedCharacter)
+    val remainingCharacters = gameBoard.handleQuestion("name", Left(selectedCharacter.name.toLowerCase()))
+    gameBoard.checkWinCondition(remainingCharacters) should be(true)
+  }
 }
